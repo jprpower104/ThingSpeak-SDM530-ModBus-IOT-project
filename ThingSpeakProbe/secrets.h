f50ef7269b0df264cd3c5f0b46fedbbfd11f32fd
Julián Peláez-Restrepo <48139834+jprpower104@users.noreply.github.com>
2020-03-19 22:27:10 -0500

    Create secrets.h
    Names and password of wifi network, and Channel and APIkey of ThingSpeak platform.

---
+++ ThingSpeakProbe/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "SSID_NAME_WIFI"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "PASSWORD_WIFI"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 0000000			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "YYXXXXXXXXXXXXZZ"   // replace XYZ with your channel write API Key
